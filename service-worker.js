5ee6dca39b4de6a363adc99f5dac9232dae5b17d
Ryan Yang <ryanqyang@gmail.com>
2020-09-18 06:59:31 -0700

    New Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2eb96b09da5da976a6c48b608182f4dd.js"
+  "/precache-manifest.f5b8e6599d6969bcba75f6f19eaa968c.js"
 );
 
 self.addEventListener('message', (event) => {
